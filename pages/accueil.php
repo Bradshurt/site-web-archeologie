8b1231cef2d02f11f676e7a22a0f5e79f29f4aff
Bradshurt <yannbradley17@gmail.com>
2025-06-17 21:08:44 +0200

    gestion des pages différentes pages

---
+++ pages/accueil.php
@@ -0,0 +1,51 @@
+<div class="main-content">
+  <div class="breaking-news">
+    <div class="first-news">
+      <div class="p-title-news">
+        <h1 class="main-title">Bienvenue sur Archéo-IT</h1>
+        <p class="main-description">
+          Découvrez les dernières avancées en archéologie et technologies de
+          l'information. Explorez nos chantiers de fouilles, participez à nos
+          événements et rejoignez notre communauté passionnée.
+        </p>
+      </div>
+      <div class="carousel" onmousemove="handleMouseMove(event)">
+        <div class="carousel-inner disposition">
+          <div class="card active">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 1</h3>
+            <p>Description de la carte 1.</p>
+          </div>
+          <div class="card">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 2</h3>
+            <p>Description de la carte 2.</p>
+          </div>
+          <div class="card">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 3</h3>
+            <p>Description de la carte 3.</p>
+          </div>
+          <div class="card">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 4</h3>
+            <p>Description de la carte 4.</p>
+          </div>
+          <div class="card">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 5</h3>
+            <p>Description de la carte 5.</p>
+          </div>
+          <div class="card">
+            <img src="/assets/img/img-card.png" alt="" class="img-card" />
+            <h3>Carte 6</h3>
+            <p>Description de la carte 6.</p>
+          </div>
+          <button class="prev" onclick="moveSlide(-1)">&#10094;</button>
+          <button class="next" onclick="moveSlide(1)">&#10095;</button>
+        </div>
+      </div>
+    </div>
+    <div class="second-news"></div>
+  </div>
+</div>
